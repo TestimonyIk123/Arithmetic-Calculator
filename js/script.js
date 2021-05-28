103000152a3d84cd3eb49f6943bd9e1858fa7119
Ikekhuamen Testimony <testimonyikekhuamen@gmail.com>
2021-05-28 08:53:35 -0700

    add initial paragraphs

---
+++ js/script.js
@@ -0,0 +1,29 @@
+function add(number1, number2) {
+  return number1 + number2;
+}
+
+function subtract(number1, number2) {
+return number1 - number2;
+}
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
+function remainer(number1, number2) {
+  return number1 % number2;
+}
+
+$(document).ready(function(){
+  $("#add").submit(function(event){
+    event.preventDefault();
+    let firstNumber=$("#add1").val();
+    let secondNumber=$("#add2").val();
+    let result=add(parseFloat(firstNumber),parseFloat(secondNumber));
+    alert(result);
+  })
+})
